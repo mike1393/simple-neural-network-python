077b54df9f9262c5207b084bf8186d323aeab3c1
mike1393 <57059936+mike1393@users.noreply.github.com>
2022-03-29 20:28:52 +0800

    Create a nerual network package
    Create a neural network package for better ogranization

---
+++ neural_network/activation_function.py
@@ -0,0 +1,13 @@
+# Third Party Packages
+import numpy as np
+# Build-in Packages
+import random
+# Local Packages
+
+# sigmoid function
+def sigmoid(z):
+    pass
+
+# the derivative of sigmoid function
+def d_sigmoid(z):
+    pass
---
+++ neural_network/neural_network.py
@@ -0,0 +1,8 @@
+# Third Party Packages
+import numpy as np
+# Build-in Packages
+import random
+# Local Packages
+from neural_network.activation_function import *
+
+
