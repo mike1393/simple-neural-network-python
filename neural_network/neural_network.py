ca44398222d20d488947398586c183c9c8f449fb
mike1393 <57059936+mike1393@users.noreply.github.com>
2022-03-31 20:10:45 +0800

    Modified NeuralNetwork
    Added a new parameter for activation selection
    Added a new function for plotting learning curve

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -1,26 +1,30 @@
 # Third Party Packages
 import numpy as np
+import matplotlib.pyplot as plt
 # Build-in Packages
 import random
 # Local Packages
-from neural_network.activation_function import sigmoid, d_sigmoid
+import neural_network.activation_function as af
 
 class NeuralNetwork:
     # class members:
     # @neurons: a list of neuron numbers at each layer
     # @weights: a list of weight matrix between layers
     # @biases: a list of bias matrix for layers[1:]
-    def __init__(self, neurons):
+    def __init__(self, neurons, af_name):
         self.neurons = neurons
         self.layer_num = len(self.neurons)
         self.weights = [np.random.randn(j,i) for i,j in zip(self.neurons[:-1], self.neurons[1:])]
         self.biases = [np.random.randn(j,1) for j in self.neurons[1:]]
+        self.af, self.d_af = af.activate_method[af_name]
+        self.learning_curve=[]
     
     def feed_forward(self, input_layer):
         # update the activation value from first layer 
         # all the way to last layer
         for weight, bias in zip(self.weights, self.biases):
-            input_layer = sigmoid(np.dot(weight, input_layer) + bias)
+            z = np.dot(weight, input_layer) + bias
+            input_layer = self.af(z)
         return input_layer
 
     # back proporgation find the gradient from input data
@@ -35,21 +39,21 @@ class NeuralNetwork:
         for weight, bias in zip(self.weights, self.biases):
             z = np.dot(weight, layer) + bias
             z_for_each_layer.append(z)
-            layer = sigmoid(z)
+            layer = self.af(z)
             layers.append(layer)
         # backward pass
         # initialize zero matrix for nabla_w and nabla_b
         nabla_w = [np.zeros(weight.shape) for weight in self.weights]
         nabla_b = [np.zeros(bias.shape) for bias in self.biases]
         # calculate d_cost_z = d_a_z * d_cost_a
-        d_cost_z = self.d_cost_a(layers[-1], y)*d_sigmoid(z_for_each_layer[-1])
+        d_cost_z = (layers[-1] - y)*self.d_af(z_for_each_layer[-1])
         # calculate the last layer nabla for weight and bias
         nabla_w[-1] = np.dot( d_cost_z, layers[-2].transpose())
         nabla_b[-1] = d_cost_z
         for idx in range(2, self.layer_num):
-            # d_cost_z^L-i = d_a_z^L-i * d_cost_a^L-1
+            # d_cost_z^L-i = d_a_z^L-i * d_cost_a^L-i
             # d_cost_a^L-i = d_z_a^L-i * d_cost_z
-            d_cost_z = np.dot(self.weights[-idx+1].transpose(), d_cost_z) * d_sigmoid(z_for_each_layer[-idx])
+            d_cost_z = np.dot(self.weights[-idx+1].transpose(), d_cost_z) * self.d_af(z_for_each_layer[-idx])
             nabla_w[-idx] = np.dot( d_cost_z, layers[-idx-1].transpose())
             nabla_b[-idx] = d_cost_z
         return (nabla_w, nabla_b)
@@ -74,6 +78,7 @@ class NeuralNetwork:
 
     # Stochastic Gradient Descent
     def sgd(self, training_data, epochs, batch_size, eta, test_data=None):
+        self.learning_curve = [0 for _ in range(epochs)]
         # get the length of the data
         number_of_training = len(training_data)
         # do the same thing for test data if it exist
@@ -92,7 +97,8 @@ class NeuralNetwork:
             # if test exist
             if test_data is not None:
                 # show training result
-                print(f"Epoch {j}: {self.evaluate(test_data)}/{number_of_test}")
+                self.learning_curve[j] = self.evaluate(test_data)
+                print(f"Epoch {j}: {self.learning_curve[j]}/{number_of_test}")
             # else show complete
             else:
                 print(f"Epoch {j} completed!")
@@ -106,7 +112,8 @@ class NeuralNetwork:
         # sum them up and return the value 
         return sum(int(y[idx]) for idx,y in testing_result)
 
-    # d cost / d a^L
-    def d_cost_a(self, output_layer, ground_truth):
-        return (output_layer - ground_truth)
+    def plot_learning_curve(self):
+        plt.plot(self.learning_curve)
+        plt.ylabel('some numbers')
+        plt.show()
     
