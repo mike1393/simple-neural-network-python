0ffb4c9a9c1ebdd44dc2417c1104345ba99ad40f
mike1393 <57059936+mike1393@users.noreply.github.com>
2022-03-31 20:09:40 +0800

    Added more activation function to the module

--- neural_network/activation_function.py
+++ neural_network/activation_function.py
@@ -1,8 +1,9 @@
 # Third Party Packages
 import numpy as np
 # Build-in Packages
-import random
-# Local Packages
+SIGMOID = "SIGMOID"
+RELU = "RELU"
+TANH="TANH"
 
 # sigmoid function
 def sigmoid(z):
@@ -13,7 +14,22 @@ def d_sigmoid(z):
     return sigmoid(z)*(1-sigmoid(z))
 
 def relu(z):
-    return max(0, z)
+    z = np.exp(z)
+    return np.where(z>0,z,1.0*(z-1))
 
 def d_relu(z):
-    return 0 if z <=0 else 1
+    z = np.exp(z)
+    return np.where(z>0,1.,1.0*z)
+
+def htan(z):
+    return (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
+
+def d_htan(z):
+    return 1-np.square(htan(z))
+
+# activation type
+activate_method={
+SIGMOID : (sigmoid, d_sigmoid),
+RELU : (relu, d_relu),
+TANH:(htan,d_htan)
+}
