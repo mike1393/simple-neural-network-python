2e05beafcbb55a674707c19cbc4f4b4107d6e04d
mike1393 <57059936+mike1393@users.noreply.github.com>
2022-03-31 16:37:59 +0800

    Added scripts to test mnist_load function and class NeuralNetwork
    Verified Functionality:
    mnist_loader()
    NeuralNetwork.sgd()

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-from neural_network.neural_network import *
+from neural_network.neural_network import NeuralNetwork
+from data_loader import mnist_loader
+
+training_set, validation_set, test_set = mnist_loader()
+net = NeuralNetwork([784,30,10])
+net.sgd(training_set, 10,10,3.0,test_data=test_set)
+
+
