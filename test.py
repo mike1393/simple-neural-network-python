1925be686fb796000c7ee986298849976ef4ec89
mike1393 <57059936+mike1393@users.noreply.github.com>
2022-04-01 01:39:53 +0800

    Visualize data result

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
+# Third Party Packages
+import matplotlib.pyplot as plt
+import numpy as np
+# Build-in Packages
+import os
+import pickle
 # Local Packages
-from neural_network.neural_network import NeuralNetwork
+from neural_network.neural_network import NeuralNetwork, model_loader
 import neural_network.activation_function as af
 from data_loader import mnist_loader
-
+output_file_path = os.path.join(os.path.dirname(__file__),'model_pkl')
 training_set, validation_set, test_set = mnist_loader()
-net = NeuralNetwork([784,30,10],af.SIGMOID)
-net.sgd(training_set, 10,10,3.0,test_data=test_set)
-net.plot_learning_curve()
+# net = NeuralNetwork([784,30,10],af.SIGMOID)
+# net.sgd(training_set, 10,10,3.0)
+# net.save(output_file_path)
+net2 = model_loader(output_file_path)
+for i in range(5):
+    x, y = test_set[i]
+    result, confidence = net2.classify(x)
+    img = np.reshape(x,(28,28))
+    fig = plt.figure
+    plt.title(f"Result: {result}, Confidence: {confidence*100}%")
+    plt.imshow(img, cmap='gray')
+    plt.show()
 
 
